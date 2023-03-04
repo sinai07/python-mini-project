605c777d0daac067e803e664a82ae90d3c4e66e2
sina-river <eguisina07@gmail.com>
2023-03-04 14:40:53 +0000

    code printed

---
+++ code-mypython.py
@@ -0,0 +1,22 @@
+# My First proyect with Python
+# Create variable for my Services list:
+a = "S3"
+b = "Lambda"
+c = "EC2"
+d = "ELB"
+e = "cloudformation"
+
+# Create an empty list:
+
+my_list = []
+
+# Create a new list with all the services added using append:
+my_list.append(a)
+my_list.append(b)
+my_list.append(c)
+my_list.append(d)
+my_list.append(e)
+
+# Print my new list and len of list:
+print(my_list)
+print(len(my_list))
